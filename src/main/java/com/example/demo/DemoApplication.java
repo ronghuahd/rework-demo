9430845e6b95992f32ee66fe48daa9fa9d63ec5c
ronghuahd <ronghuahd@gmail.com>
2021-03-20 00:17:17 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("==========启动Springboot ==========");
+        System.out.println("测试Jenkins拉取");
     }
 
 }
